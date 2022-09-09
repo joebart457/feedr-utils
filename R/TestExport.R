bd3e97acc474658f8f6a0cf480ae983bb7729bb1
Joe Bart <joebart457@yahoo.com>
2022-09-09 14:46:25 -0700

    test2

--- R/TestExport.R
+++ R/TestExport.R
@@ -1,3 +1,3 @@
 load_all <- function() {
-	TestData <- read.csv(concat(system.file(package = "FeedrRewrite"), "extdata/MyData.csv"))
+	TestData <- read.csv(paste(system.file(package = "FeedrRewrite"), "/extdata/MyData.csv"))
 }
